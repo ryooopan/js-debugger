7989b4c292c96511f99138c3c75481c5868940a4
Ryo Suzuki <ryooopan@googlemail.com>
2015-01-07 17:23:24 +0900

    add requirejs with bower

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,10 @@ exports.sandbox = function(req, res) {
   var Sandbox = require('sandbox'),
       sandbox = new Sandbox();
 
+  var bodyParser = require('body-parser');
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyparser.json());
   console.log(req);
   sandbox.run('1+1', function(output) {
     res.send(output);
